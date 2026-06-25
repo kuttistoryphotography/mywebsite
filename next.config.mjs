117e8b1112067219900b4c516b3c27f494409585
Kuttistory <Kuttistoryphotography@gmail.com>
2026-06-25 16:45:15 +0530

    Add dynamic portfolio sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,41 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  async redirects() {
+    return [
+      {
+        source: "/outdoor-couple-photoshoot",
+        destination: "/works",
+        permanent: true,
+      },
+      {
+        source: "/films",
+        destination: "/services",
+        permanent: true,
+      },
+      {
+        source: "/festivals",
+        destination: "/works",
+        permanent: true,
+      },
+      {
+        source: "/baby-shower-photoshoot",
+        destination: "/baby-shoot-photography-in-madurai",
+        permanent: true,
+      },
+      {
+        source: "/faq-kutti-story-photography",
+        destination: "/faq",
+        permanent: true,
+      },
+      {
+        source: "/contact-us",
+        destination: "/contact-us",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
