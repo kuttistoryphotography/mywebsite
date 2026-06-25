3f23e3e95e139d16834c82d46e5e25bc94efc526
Kuttistory <Kuttistoryphotography@gmail.com>
2026-06-25 13:13:22 +0530

    Add dynamic portfolio sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import Script from "next/script";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import "./globals.css";
 import ConditionalLayout from "../components/ConditionalLayout";
 
 const geistSans = Geist({
