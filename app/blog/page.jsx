15a7f1a569d5cfb729f0556e3b4bf063de0d4d68
Kuttistory <Kuttistoryphotography@gmail.com>
2026-06-26 13:49:15 +0530

    Add dynamic portfolio sitemap

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,13 +1,15 @@
 "use client";
 
-import BlogSection from "../../components/./blog/BlogSection";
+import BlogSection from "../../components/blog/BlogSection";
 
-export default function AboutUsPage() {
+export default function BlogPage() {
   return (
     <>
+      <h1 className="sr-only">
+        Wedding Photography Blog | Kutti Story Photography
+      </h1>
 
       <BlogSection />
-
     </>
   );
 }
