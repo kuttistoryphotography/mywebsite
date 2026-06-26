09a95c6ee9498f76e15719db924fa43d4861c42e
Kuttistory <Kuttistoryphotography@gmail.com>
2026-06-26 12:46:33 +0530

    Add dynamic portfolio sitemap

--- components/home/HomeFirst.tsx
+++ components/home/HomeFirst.tsx
@@ -1,50 +1,28 @@
 "use client";
 
-
 import Hero from "./Hero";
 import AboutSection from "./AboutSection";
 import WeddingShowcase from "./WeddingShowcase";
 import PhilosophySection from "./PhilosophySection";
 import FeaturedWork from "./FeaturedWork";
 import StatsCardsSection from "./StatsCardsSection";
-import StoriesSection from "./StoriesSection";
-import WeddingCTA from "./WeddingCTA";
-
-interface HeroData {
-  backgroundImage: string;
-  heading: string;
-  subheading: string;
-  paragraph: string;
-  badgeText: string;
-  primaryButtonText: string;
-  secondaryButtonText: string;
-  statsYears: string;
-  statsStories: string;
-  statsPassion: string;
-  heroCardImage: string;
-  awardText: string;
-}
-
-interface HomeFirstProps {
-  settings: {
-    hero: HeroData;
-  };
-}
-
-export default function HomeFirst({ settings }: HomeFirstProps) {
+import StoriesSection from"./StoriesSection";
 
 
+export default function HomeFirst() {
   return (
     <>
-      <Hero hero={settings.hero} />
+    
 
+      <Hero />
       <AboutSection />
       <WeddingShowcase />
       <PhilosophySection />
       <FeaturedWork />
       <StatsCardsSection />
-      <StoriesSection />
-      <WeddingCTA />
+      <StoriesSection  />
+
+      
     </>
   );
 }
