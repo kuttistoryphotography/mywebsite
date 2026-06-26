af2c049eca7e638060b9574914f3c90ce01bc5b5
Kuttistory <Kuttistoryphotography@gmail.com>
2026-06-26 12:47:03 +0530

    Add dynamic portfolio sitemap

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,18 +1,5 @@
 import HomeFirst from "../../components/home/HomeFirst";
 
-async function getHomepage() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/homepage`,
-    {
-      cache: "no-store",
-    }
-  );
-
-  return res.json();
-}
-
-export default async function HomePage() {
-  const { settings } = await getHomepage();
-
-  return <HomeFirst settings={settings} />;
+export default function HomePage() {
+  return <HomeFirst />;
 }
