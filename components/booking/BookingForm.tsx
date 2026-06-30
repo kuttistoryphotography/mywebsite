96859f5c35063cfb8b42983d9a19325408a382c3
Kuttistory <Kuttistoryphotography@gmail.com>
2026-06-30 14:14:13 +0530

    Add dynamic portfolio sitemap

--- components/booking/BookingForm.tsx
+++ components/booking/BookingForm.tsx
@@ -459,10 +459,11 @@ export default function BookingForm({ onSuccess, isDialog = false }: { onSuccess
   }
 
   return (
-    <section className={isDialog ? "bg-transparent" : "min-h-screen bg-zinc-950 flex"}>
+    
+<section className={ isDialog ? "bg-transparent" : "min-h-screen bg-zinc-950 flex pt-24 lg:pt-0" } >
       {/* Left Side - Image */}
       {!isDialog && (
-        <div className="hidden lg:block lg:w-1/2 relative">
+        <div className="hidden lg:block lg:w-1/2 relative pt-24 lg:pt-0">
           <div className="absolute inset-0 bg-gradient-to-r from-transparent to-zinc-950/50 z-10" />
           <img
             src={
